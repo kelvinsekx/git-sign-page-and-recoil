d1df544e51b9f083f79ffd71dad02635282c9c50
kelvinsekx <utdkelvin@gmail.com>
2021-08-27 15:09:19 +0100

    working fine now

--- src/components/reactish-utils.js
+++ src/components/reactish-utils.js
@@ -6,7 +6,7 @@ export const Input = ({For, onChange, st})=>
 <StyledInput>
 	{For == "username" ? 
 	<div style={{textAlign: "left",fontSize: "80%", paddingBottom:"0.2em"}}>Username or email address</div> :
-	<div style={{display: "flex", fontSize: "80%",justifyContent: "space-between"}}>
+	<div style={{paddingBottom: "0.2em",display: "flex", fontSize: "80%",justifyContent: "space-between"}}>
 		<span>Password</span>
 	<span style={{color: '#54B4F1'}}>forgot password?</span>
 	</div>}
@@ -39,6 +39,8 @@ export const SubmitBtn = ({onSubmit, done})=>
 const StyledBtn = styled.div`
 button{
     width: 16rem;
+    font-size: 95%;
+//	height: 70%;
     background-color: green;
     border-radius: 5px;
 }
